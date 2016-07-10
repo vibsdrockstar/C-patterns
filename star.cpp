69d72da57faf8c8770050317991da5a3f4f3ff9b
Vibhor <bijoy.vibhor19@gmail.com>
2016-07-10 17:07:51 +0530

    Add files via upload

---
+++ star.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j,rows;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+    for(i=rows;i>=1;--i)
+    {
+        for(j=1;j<=i;++j)
+        {
+           cout<<j;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
+
